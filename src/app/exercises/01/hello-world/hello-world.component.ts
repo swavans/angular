116a65b16fffc1c02bda3fd05ec9ada5031af69f
Sascha Worms <sascha.worms@amadeus-hospitality.net>
2017-08-23 11:59:37 +0200

    added time

--- src/app/exercises/01/hello-world/hello-world.component.ts
+++ src/app/exercises/01/hello-world/hello-world.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HelloWorldComponent implements OnInit {
 
   year  = new Date().getFullYear();
-  month = new Date().getMonth();
+  month = new Date().getMonth() + 1;
+  day = new Date().getDate();
+  hour = new Date().getHours();
+  minutes = new Date().getMinutes();
+  seconds = new Date().getSeconds();
   constructor() { }
 
   ngOnInit() {
